622c49f715d5a1884c5f27fca273e649beb8a248
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2025-09-30 13:54:59 -0700

    feat: basic fetch for movie api (#2)
    * feat: basic fetch for movie api
    
    * refactor: generic fetch to movie API
    
    * refactor: basic fetch to movie api

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hi there!');
+const movieURL = "http://www.omdbapi.com";
+const apiKey = "cf6ab924";
+
+const fetchData = async (params) => {
+  try {
+    const url = new URL(movieURL);
+    url.search = new URLSearchParams({
+      apikey: apiKey,
+      ...params,
+    });
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      // Throw an error to be caught by the catch block
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Could not fetch data:", error);
+    return null;
+  }
+};
+
+const main = async () => {
+  const searchResults = await fetchData({ s: "avengers" }); //"index" search
+  console.log("Search results:", searchResults);
+
+  const movieDetails = await fetchData({ i: "tt0848228" }); //"show" search
+  console.log("Movie details:", movieDetails);
+};
+
+main();
