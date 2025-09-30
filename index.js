2cf69368013ea2de468474e157af533b2d198454
Valerie Racine <vracine21@gmail.com>
2025-09-30 13:26:54 -0700

    feat: basic fetch for movie api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const movieURL = "http://www.omdbapi.com";
+const apiKey = "/?apikey=cf6ab924&";
+const search = "s=avengers";
+const movieId = "i=tt0848228";
+
+const fetchData = async () => {
+  const response = await fetch(movieURL + apiKey + movieId);
+  const data = await response.json();
+  console.log(data);
+};
+
+fetchData();
